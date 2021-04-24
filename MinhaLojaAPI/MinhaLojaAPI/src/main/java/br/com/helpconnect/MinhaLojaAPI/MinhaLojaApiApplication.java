13eb7ba4979b1f20c4a4ecf09b956f488f089615
Clamant96 <kevin.lazzarotto@hotmail.com>
2021-04-24 00:00:33 -0300

    Criacao de projeto Minha Loja API

---
+++ MinhaLojaAPI/MinhaLojaAPI/src/main/java/br/com/helpconnect/MinhaLojaAPI/MinhaLojaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.helpconnect.MinhaLojaAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhaLojaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhaLojaApiApplication.class, args);
+	}
+
+}
---
+++ MinhaLojaAPI/MinhaLojaAPI/src/test/java/br/com/helpconnect/MinhaLojaAPI/MinhaLojaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.helpconnect.MinhaLojaAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhaLojaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
